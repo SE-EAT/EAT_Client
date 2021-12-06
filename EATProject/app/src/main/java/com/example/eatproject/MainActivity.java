2a35a7b4b166dbd50e788592e1d698610ff09367
gyumin <rbals9000@gmail.com>
2021-12-06 13:32:26 +0900

    add user

--- EATProject/app/src/main/java/com/example/eatproject/MainActivity.java
+++ EATProject/app/src/main/java/com/example/eatproject/MainActivity.java
@@ -21,27 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        inpId = findViewById(R.id.loginId);
-        inpId.setSelection(inpId.length());
-
-        btn_register = findViewById(R.id.register_btn);
-        btn_search = findViewById(R.id.search_btn);
+        btn_register = findViewById(R.id.buttonRegister);
         //회원가입버튼 클릭 시 회원가입 화면으로 이동
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Register.class);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
         });
 
-        //아이디/PW 찾기 버튼 클릭시 이동
-        btn_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SearchInfo.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
--- EATProject/app/src/main/java/com/example/eatproject/RegisterActivity.java
+++ EATProject/app/src/main/java/com/example/eatproject/RegisterActivity.java
@@ -2,13 +2,52 @@ package com.example.eatproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+//회원가입 Activity
 public class RegisterActivity extends AppCompatActivity {
+    private Button btn_request_register;
+    private EditText etxt_mail, etxt_id, etxt_pw, etxt_nickname, etxt_address;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int selectedID;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btn_request_register= findViewById(R.id.buttonRequestRegister);
+        etxt_mail = findViewById(R.id.editTextEmailAddress);
+        etxt_id = findViewById(R.id.editTextRegisterID);
+        etxt_pw = findViewById(R.id.editTextRegisterPW);
+        etxt_nickname = findViewById(R.id.editTextRegisterNickname);
+        etxt_address = findViewById(R.id.editTextRegisterAddress);
+        rg = (RadioGroup) findViewById(R.id.radioGroup);
+
+        btn_request_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = etxt_mail.getText().toString();
+                String id = etxt_id.getText().toString();
+                String pw = etxt_pw.getText().toString();
+                String nickname = etxt_nickname.getText().toString();
+                String address = etxt_nickname.getText().toString();
+                RadioButton rd = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+                String str_Qtype = rd.getText().toString();
+                User user = new User(mail, id, pw, nickname, address, str_Qtype);
+                Toast.makeText(getApplicationContext(),"회원가입 완료",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
